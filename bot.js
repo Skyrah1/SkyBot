bafd2b3733510f784f82acc249bd2b6fd19dce8a
Skyrah1 <zhunchenyek1@gmail.com>
2019-11-12 22:37:43 +0000

    IT'S ALIIIIIIIIIVE
    npm install discord.js

--- bot.js
+++ bot.js
@@ -1,17 +1,23 @@
 const fs = require("fs");
-
-var fileName = "loginToken"
-
-var output = fs.readFileSync(fileName, "utf-8", (err, data) => {
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const fileName = "loginToken";
+const token = fs.readFileSync(fileName, "utf-8", (err, data) => {
     if (err){
         return err;
     } else {
         return data;
     }
-})
+});
 
-function main(){
-    console.log(output);
-}
+client.on("ready", () => {
+    console.log("Logged in as ${client.user.tag}!")
+});
 
-main();
+client.on("message", msg => {
+    if (msg.content === "!helloWorld"){
+        msg.reply("Hello ZA WARUDO\nTOKI GA TOMARE!!!");
+    }
+});
+
+client.login(token);
